0427e677b9741a678da5a9a74eb362c89f747ec4
7rans <transfire@gmail.com>
2012-01-24 20:53:36 -0500

    Yeah for tests. [test]

---
+++ test/case_brass.rb
@@ -0,0 +1,22 @@
+covers 'brass'
+
+testcase Kernel do
+
+  method :assert do
+
+    test do
+      assert(true)
+    end
+
+  end
+
+  method :refute do
+
+    test do
+      refute(false)
+    end
+
+  end
+
+end
+
