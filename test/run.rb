8ca30890ce489a6d4c734c4a57eccce280a1d057
7rans <transfire@gmail.com>
2014-07-19 08:55:33 -0400

    Add test runner script. :admin:

--- test/run.rb
+++ test/run.rb
@@ -1,7 +1,8 @@
 require 'rubytest'
 require 'lemon'
+require 'rubytest/autorun'
 
-Test.run(:default) do |run|
+Test.run do |run|
   run.loadpath 'lib'
   run.files << 'test/case_*.rb'
 end
@@ -9,6 +10,7 @@ end
 Test.run(:cov) do |run|
   run.loadpath 'lib'
   run.files << 'test/case_*.rb'
+  require 'simplecov'
   SimpleCov.start do |cov|
     cov.coverage_dir = 'log/coverage'
   end
